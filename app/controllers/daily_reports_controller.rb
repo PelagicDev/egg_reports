aeca3cf3b840caa3bdce8ecd16443e9c187b1d75
Jordan Godwin <jordan@panurgic.net>
2014-10-06 22:18:26 -0400

    Good start on getting the nested resources working

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,18 +1,20 @@
 class DailyReportsController < ApplicationController
-  before_action :set_daily_report, only: [:show, :edit, :update, :destroy]
+  # before_action :set_daily_report, only: [:show, :edit, :update, :destroy]
+  before_filter :get_house
 
-  # GET /daily_reports
+  # GET houses/1/daily_reports
   # GET /daily_reports.json
   def index
-    @daily_reports = DailyReport.all
+    @daily_reports = @house.daily_reports
   end
 
   # GET /daily_reports/1
   # GET /daily_reports/1.json
   def show
+    @daily_report = @house.daily_reports.find(params[:id])
   end
 
-  # GET /daily_reports/new
+  # GET houses/1/daily_reports/new
   def new
     @daily_report = DailyReport.new
   end
@@ -24,12 +26,12 @@ class DailyReportsController < ApplicationController
   # POST /daily_reports
   # POST /daily_reports.json
   def create
-    @daily_report = DailyReport.new(daily_report_params)
+    @daily_report = @house.daily_reports.new(daily_report_params)
 
     respond_to do |format|
       if @daily_report.save
-        format.html { redirect_to @daily_report, notice: 'Daily report was successfully created.' }
-        format.json { render :show, status: :created, location: @daily_report }
+        format.html { redirect_to [@house, @daily_report], notice: 'Daily report was successfully created.' }
+        format.json { render :show, status: :created, location: [@house, @daily_report] }
       else
         format.html { render :new }
         format.json { render json: @daily_report.errors, status: :unprocessable_entity }
@@ -62,10 +64,14 @@ class DailyReportsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_daily_report
-      @daily_report = DailyReport.find(params[:id])
+
+    def get_house
+      @house = House.find(params[:house_id])
     end
+
+    # def set_daily_report
+    #   @daily_report = DailyReport.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def daily_report_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
 
   root 'landings#home'
 
@@ -6,11 +7,10 @@ Rails.application.routes.draw do
 
   resources :flocks
 
-  resources :houses
+  resources :houses, shallow: true do
+    resources :daily_reports
+  end
 
-  resources :daily_reports
-
-  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
